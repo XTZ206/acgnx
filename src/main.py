69f2b8d19a799fcaacfd2f7623ff323bf9f5a47f
XTZ206 <ywx2006@outlook.com>
2025-03-07 14:17:23 +0800

    feat: rearrange parser function and parameters

--- src/main.py
+++ src/main.py
@@ -11,70 +11,79 @@ def main():
         prog="acgnx", usage="%(prog)s [command]", description="ACGN indeX v0.0.1"
     )
     subparsers = argparser.add_subparsers(dest="command")
-    
-    list_parser = subparsers.add_parser("list", help="list subjects")
-    view_parser = subparsers.add_parser("view", help="view subject with id")
-    view_parser.add_argument("id", type=int, help="view subject with id")
-    view_parser.add_argument("-f", "--fetch", action="store_true", help="fetch subject before viewing")
-    view_parser.add_argument("-c", "--count", type=int, help="count of infobox items to show", default=5)
-    update_parser = subparsers.add_parser("update", help="update subjects")
-    fetch_parser = subparsers.add_parser("fetch", help="fetch subjects")
-    fetch_parser.add_argument("id", type=int, help="fetch subject with id")
-    search_parser = subparsers.add_parser("search", help="search subjects with name")
-    search_parser.add_argument("keyword", type=str, help="search keyword")
-    search_parser.add_argument("--online", action="store_true", help="search subjects from bgm.tv")
-    search_parser.add_argument("--store", action="store_true", help="store search results to database")
 
+    list_parser = subparsers.add_parser(
+        "list", help="list subjects based on subject name and aliases"
+    )
+    list_parser.add_argument("name", nargs="?", type=str, default="")
+    view_parser = subparsers.add_parser("view", help="view subject with id")
+    view_parser.add_argument("id", type=int, help="to-be-viewed subject id")
+    update_parser = subparsers.add_parser(
+        "update", help="update specified subject based on subject id"
+    )
+    update_parser.add_argument("id", type=int, help="to-be-updated subject id")
+    fetch_parser = subparsers.add_parser(
+        "fetch", help="fetch subject based on subject id"
+    )
+    fetch_parser.add_argument("id", type=int, help="to-be-fetched subject id")
+    search_parser = subparsers.add_parser("search", help="search subjects from bgm.tv")
+    search_parser.add_argument("keyword", type=str, help="search keyword")
     args = argparser.parse_args()
-    
+
     match args.command:
-        
+
         case "list":
-            viewer = view.Viewer(dbhandler.fetch_all_subjects())
+            if args.name == "":
+                viewer = view.Viewer(dbhandler.fetch_all_subjects())
+            else:
+                viewer = view.Viewer(dbhandler.search_subjects(args.name))
             viewer.list_subjects()
             return
 
         case "view":
-
-            if args.fetch:
-                viewer = view.Viewer([Subject(id=args.id)], view.Updater(apihandler))
-                viewer.update_subjects()
-                dbhandler.insert_subjects(viewer.subjects)
-            else:
-                viewer = view.Viewer([Subject(id=args.id)], view.Updater(dbhandler))
-                viewer.update_subjects()
-            viewer.view_subject(viewer.subjects[0], args.count)
+            viewer = view.Viewer([Subject(id=args.id)], view.Updater(dbhandler))
+            viewer.view_subject()
             return
 
         case "update":
-            viewer = view.Viewer(dbhandler.fetch_all_subjects(), view.Updater(apihandler))
-            viewer.update_subjects()
-            dbhandler.update_subjects(viewer.subjects)
-            viewer.list_subjects()
-            print("All subjects updated")
-            return
+            if args.id is None:
+                viewer = view.Viewer(
+                    dbhandler.fetch_all_subjects(), view.Updater(apihandler)
+                )
+                viewer.update_subjects()
+                dbhandler.update_subjects(*viewer.subjects)
+                viewer.list_subjects()
+                print("All subjects updated")
+                return
+            else:
+                viewer = view.Viewer(dbhandler.fetch_subject(args.id))
+                viewer.list_subject()
+                print("All required subjects updated")
+                return
 
         case "fetch":
-            viewer = view.Viewer([Subject(id=args.id)], view.Updater(apihandler))
+            updater = view.Updater(apihandler)
+            viewer = view.Viewer([Subject(args.id)], updater)
             viewer.update_subjects()
-            dbhandler.insert_subjects(viewer.subjects)
-            return              
+            dbhandler.insert_subjects(*viewer.subjects)
+            viewer.list_subjects()
+            print("All required subject fetched")
 
         case "search":
-            
-            updater = view.Updater(apihandler) if args.online else view.Updater(dbhandler)
+            updater = view.Updater(apihandler)
             viewer = view.Viewer([], updater, view.Selector())
             viewer.search_subjects(args.keyword)
             viewer.list_subjects()
-            if args.store:
-                dbhandler.insert_subjects(viewer.subjects)
-            print("Viewing search results")
+            print("Viewing searching results")
             return
-            
+
+        case "help":
+            argparser.print_help()
+            return
 
         case _:
             argparser.print_help()
-            return          
+            return
 
 
 if __name__ == "__main__":
